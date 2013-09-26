9b4ee32e2ebbba75700f1b23f5bc11316844278c
bloognoo <ethics@moonforge.com>
2013-09-26 23:07:14 +0100

    new file:   Impetus.sln 	new file:   Impetus/Assets/openfl.svg 	new file:   Impetus/Impetus.oflproj 	new file:   Impetus/Source/Impetus.hx 	new file:   Impetus/project.xml

---
+++ Impetus/Source/Impetus.hx
@@ -0,0 +1,18 @@
+package;
+
+import flash.display.Sprite;
+import flash.Lib;
+
+/**
+ * ...
+ * @author bloognoo
+ */
+class Impetus extends Sprite {
+	
+	public function new () {
+
+		super ();
+
+	}
+
+}
